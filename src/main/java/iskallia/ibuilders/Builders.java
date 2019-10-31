67bfedab7cc6269985b75f8a5bbd783c8a929d75
Anılcan <igoodie@programmer.net>
2019-11-01 02:02:24 +0300

    Initial commit

---
+++ src/main/java/iskallia/ibuilders/Builders.java
@@ -0,0 +1,55 @@
+package iskallia.ibuilders;
+
+import iskallia.ibuilders.event.EventMod;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Builders.MOD_ID, name = Builders.MOD_NAME, version = Builders.MOD_VERSION)
+public class Builders {
+
+    public static final Logger LOG = LogManager.getLogger(Builders.MOD_NAME);
+
+    @Mod.Instance
+    private static Builders INSTANCE;
+
+    public static final String MOD_ID = "ibuilders";
+    public static final String MOD_NAME = "iBuilders";
+    public static final String MOD_VERSION = "${version}";
+
+    public static Builders getInstance() {
+        return INSTANCE;
+    }
+
+    public static ResourceLocation getResource(String name) {
+        return new ResourceLocation(Builders.MOD_ID, name);
+    }
+
+    @Mod.EventHandler
+    public void onConstruction(FMLConstructionEvent event) {
+        EventMod.onConstruction(event);
+    }
+
+    @Mod.EventHandler
+    public void onPreInitialization(FMLPreInitializationEvent event) {
+        EventMod.onPreInitialization(event);
+    }
+
+    @Mod.EventHandler
+    public void onInitialization(FMLInitializationEvent event) {
+        EventMod.onInitialization(event);
+    }
+
+    @Mod.EventHandler
+    public void onPostInitialization(FMLPostInitializationEvent event) {
+        EventMod.onPostInitialization(event);
+    }
+
+    @Mod.EventHandler
+    public void onServerStart(FMLServerStartingEvent event) {
+        EventMod.onServerStart(event);
+    }
+
+}
---
+++ src/main/java/iskallia/ibuilders/event/EventMod.java
@@ -0,0 +1,19 @@
+package iskallia.ibuilders.event;
+
+import net.minecraftforge.fml.common.event.*;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+
+public class EventMod {
+
+    public static void onConstruction(FMLConstructionEvent event) { }
+
+    public static void onPreInitialization(FMLPreInitializationEvent event) { }
+
+    public static void onInitialization(FMLInitializationEvent event) { }
+
+    public static void onPostInitialization(FMLPostInitializationEvent event) { }
+
+    public static void onServerStart(FMLServerStartingEvent event) { }
+
+}
