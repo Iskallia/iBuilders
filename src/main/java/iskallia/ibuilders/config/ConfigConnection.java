e9b37844bc5f87a84be62039ce7f5bcc24ef19b8
Unknown <kaptainwutax@gmail.com>
2019-11-01 19:15:04 -0400

    simplified connection logic

--- src/main/java/iskallia/ibuilders/config/ConfigConnection.java
+++ src/main/java/iskallia/ibuilders/config/ConfigConnection.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 public class ConfigConnection extends Config {
 
+    @Expose public boolean IS_PLOT_SERVER;
     @Expose public int HOST_PORT = 0;
-    @Expose public List<NetAddress> MAIN_SERVERS = new ArrayList<>();
     @Expose public List<NetAddress> PLOT_SERVERS = new ArrayList<>();
 
     @Override
@@ -19,7 +19,17 @@ public class ConfigConnection extends Config {
 
     @Override
     protected void resetConfig() {
+        /*
+        * The FunCraft live server would have a config like such:
+        * IS_PLOT_SERVER = false;
+        * PLOT_SERVERS.add(new NetAddress("PLOT_SERVER_IP", 5000)); //IP and "HOST_PORT" of the plot server.
+        * */
 
+        /*
+        * The Plot server would have a config like such:
+        * IS_PLOT_SERVER = true;
+        * HOST_PORT = 5000; //On what port will the plot server expect connections from the live server(s)?
+        * */
     }
 
 }
--- src/main/java/iskallia/ibuilders/net/NetworkThread.java
+++ src/main/java/iskallia/ibuilders/net/NetworkThread.java
@@ -22,23 +22,16 @@ public class NetworkThread extends Thread {
     @Override
     public void run() {
         //If the main a server plans to connect, start the server listener.
-        if(!InitConfig.CONFIG_CONNECTION.MAIN_SERVERS.isEmpty()) {
+        if(InitConfig.CONFIG_CONNECTION.IS_PLOT_SERVER) {
             this.serverListener = new ServerListener(InitConfig.CONFIG_CONNECTION.HOST_PORT);
 
             this.serverListener.onConnectionEstablished(listener -> {
-                Builders.LOG.debug("Main server [" + listener.getConnectionAddress() + "] is trying to connect to this plot world.");
-                if(InitConfig.CONFIG_CONNECTION.MAIN_SERVERS.contains(listener.getConnectionAddress())) {
-                    //This main server is allowed to connect to the plot world!
-                    Builders.LOG.warn("Main server [" + listener.getConnectionAddress() + "] was allowed access.");
-                } else {
-                    Builders.LOG.warn("Main server [" + listener.getConnectionAddress() + "] was denied access, disconnecting.");
-                    listener.disconnect();
-                }
+                Builders.LOG.warn("Main server [" + listener.getConnectionAddress() + "] connected to this plot world.");
             });
 
             this.serverListener.start();
-            Builders.LOG.debug("Listener started and is awaiting for this plot world. " +
-                    "Expecting main servers: " + InitConfig.CONFIG_CONNECTION.MAIN_SERVERS + ".");
+
+            Builders.LOG.warn("Listener started and is awaiting.");
         }
 
         while(true) {
@@ -56,14 +49,14 @@ public class NetworkThread extends Thread {
                 }
 
                 //If the plot server can't be reached, try again every 20 seconds.
-                if(!connected && this.sleepCounter % 20 * 20 == 0) {
+                if(!connected && this.sleepCounter % (20 * 20) == 0) {
                     Listener listener = new Listener(address.getIp(), address.getPort());
                     listener.start();
 
-                    Builders.LOG.debug("Attempting to connect to plot server [" + listener.getConnectionAddress() + "]...");
+                    Builders.LOG.warn("Attempting to connect to plot server [" + listener.getConnectionAddress() + "]...");
 
                     listener.onConnectionEstablished(connectedListener -> {
-                        Builders.LOG.debug("Successfully connected to plot server [" + listener.getConnectionAddress() + "]!");
+                        Builders.LOG.warn("Successfully connected to plot server [" + listener.getConnectionAddress() + "]!");
                     });
 
                     this.listeners.add(listener);
