d23d487111c2b6f082b331a097c0de25ab666f3c
Anılcan <igoodie@programmer.net>
2019-11-01 21:52:38 +0300

    feat(marker): add closest querying logic

--- src/main/java/iskallia/ibuilders/block/BlockMarker.java
+++ src/main/java/iskallia/ibuilders/block/BlockMarker.java
@@ -1,6 +1,7 @@
 package iskallia.ibuilders.block;
 
 import iskallia.ibuilders.Builders;
+import iskallia.ibuilders.init.InitBlock;
 import iskallia.ibuilders.tab.CreativeTabsIBuilders;
 import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.material.Material;
@@ -8,11 +9,9 @@ import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
+import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -23,6 +22,10 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import javax.annotation.Nullable;
 
 public class BlockMarker extends BlockDirectional {
+
+    public static final int Z_LIMIT = 10;
+    public static final int X_LIMIT = 10;
+    public static final int Y_LIMIT = 10;
 
     protected static final AxisAlignedBB VERTICAL_AABB = new AxisAlignedBB(0.375D, 0.0D, 0.375D, 0.625D, 1.0D, 0.625D);
     protected static final AxisAlignedBB NS_AABB = new AxisAlignedBB(0.375D, 0.375D, 0.0D, 0.625D, 0.625D, 1.0D);
@@ -55,6 +58,73 @@ public class BlockMarker extends BlockDirectional {
 
     public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
         return state.withProperty(FACING, mirrorIn.mirror((EnumFacing) state.getValue(FACING)));
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        if (!world.isRemote && hand == EnumHand.MAIN_HAND) {
+            BlockPos closestX = closestMarkerX(world, pos);
+            BlockPos closestY = closestMarkerY(world, pos);
+            BlockPos closestZ = closestMarkerZ(world, pos);
+
+            if (closestX == null || closestY == null || closestZ == null)
+                return true;
+
+            System.out.printf("X:%s Y:%s Z:%s\n", closestX, closestY, closestZ);
+        }
+
+        return super.onBlockActivated(world, pos, state, player, hand, facing, hitX, hitY, hitZ);
+    }
+
+    private BlockPos closestMarkerY(World world, BlockPos pos) {
+        for (int i = 1; i < Y_LIMIT; i++) {
+            BlockPos positivePos = pos.add(0, i, 0);
+            BlockPos negativePos = pos.add(0, -i, 0);
+
+            if (world.getBlockState(positivePos).getBlock() == InitBlock.MARKER) {
+                return positivePos;
+            }
+
+            if (world.getBlockState(negativePos).getBlock() == InitBlock.MARKER) {
+                return negativePos;
+            }
+        }
+
+        return null;
+    }
+
+    private BlockPos closestMarkerX(World world, BlockPos pos) {
+        for (int i = 1; i < X_LIMIT; i++) {
+            BlockPos positivePos = pos.add(i, 0, 0);
+            BlockPos negativePos = pos.add(-i, 0, 0);
+
+            if (world.getBlockState(positivePos).getBlock() == InitBlock.MARKER) {
+                return positivePos;
+            }
+
+            if (world.getBlockState(negativePos).getBlock() == InitBlock.MARKER) {
+                return negativePos;
+            }
+        }
+
+        return null;
+    }
+
+    private BlockPos closestMarkerZ(World world, BlockPos pos) {
+        for (int i = 1; i < Z_LIMIT; i++) {
+            BlockPos positivePos = pos.add(0, 0, i);
+            BlockPos negativePos = pos.add(0, 0, -i);
+
+            if (world.getBlockState(positivePos).getBlock() == InitBlock.MARKER) {
+                return positivePos;
+            }
+
+            if (world.getBlockState(negativePos).getBlock() == InitBlock.MARKER) {
+                return negativePos;
+            }
+        }
+
+        return null;
     }
 
     @Nullable
