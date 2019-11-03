42b04e7c85b75bb945e4fa13e30faeb92dd5baae
Unknown <kaptainwutax@gmail.com>
2019-11-03 16:34:32 -0500

    removed debug line

--- src/main/java/iskallia/ibuilders/init/InitPacket.java
+++ src/main/java/iskallia/ibuilders/init/InitPacket.java
@@ -6,15 +6,13 @@ import iskallia.ibuilders.net.packet.PacketRequestSchematic;
 import iskallia.ibuilders.net.packet.PacketS2CDisconnect;
 import iskallia.ibuilders.net.packet.util.PacketRegistry;
 
-import java.util.Random;
-
 public class InitPacket {
 
     public static void registerPackets() {
         PacketRegistry.registerPacket(PacketC2SHandshake.class);
         PacketRegistry.registerPacket(PacketS2CDisconnect.class);
-        if(new Random().nextBoolean())PacketRegistry.registerPacket(PacketRequestSchemaInfo.class);
-        if(new Random().nextBoolean())PacketRegistry.registerPacket(PacketRequestSchematic.class);
+        PacketRegistry.registerPacket(PacketRequestSchemaInfo.class);
+        PacketRegistry.registerPacket(PacketRequestSchematic.class);
     }
 
 
