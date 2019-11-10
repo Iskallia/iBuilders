7f6edb5fd23d4d6e4ae7196c8f92935fe1db41f8
Unknown <kaptainwutax@gmail.com>
2019-11-10 12:44:00 -0500

    added creator action packet

--- src/main/java/iskallia/ibuilders/net/packet/mc/C2SCreatorAction.java
+++ src/main/java/iskallia/ibuilders/net/packet/mc/C2SCreatorAction.java
@@ -2,28 +2,39 @@ package iskallia.ibuilders.net.packet.mc;
 
 import io.netty.buffer.ByteBuf;
 import iskallia.ibuilders.Builders;
+import iskallia.ibuilders.container.ContainerCreator;
+import iskallia.ibuilders.container.ContainerSchematicTerminal;
+import iskallia.ibuilders.init.InitItem;
+import iskallia.ibuilders.item.ItemBlueprint;
 import iskallia.ibuilders.net.packet.PacketRequestSchemaInfo;
+import iskallia.ibuilders.schematic.BuildersSchematic;
+import iskallia.ibuilders.world.data.DataSchematics;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.Container;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class C2SCreatorAction implements IMessage {
 
-    private C2STerminalAction.Action action;
+    private Action action;
     private String[] args;
 
     public C2SCreatorAction() {
     }
 
-    public C2SCreatorAction(C2STerminalAction.Action action, String... args) {
+    public C2SCreatorAction(Action action, String... args) {
         this.action = action;
         this.args = args;
     }
 
     @Override
     public void fromBytes(ByteBuf buf) {
-        this.action = C2STerminalAction.Action.values()[buf.readInt()];
+        this.action = Action.values()[buf.readInt()];
 
         int size = buf.readShort();
         this.args = new String[size];
@@ -45,13 +56,42 @@ public class C2SCreatorAction implements IMessage {
     }
 
     public enum Action {
-        GET_INFO
+        GET_INFO, PRINT
     }
 
     public static class Handler implements IMessageHandler<C2SCreatorAction, IMessage> {
         @Override
         public IMessage onMessage(C2SCreatorAction message, MessageContext ctx) {
-            Builders.NETWORK.sendToAllPlotServers(new PacketRequestSchemaInfo());
+            EntityPlayerMP player = ctx.getServerHandler().player;
+            Container container = player.openContainer;
+
+            if(container instanceof ContainerCreator) {
+                DataSchematics dataSchematics = DataSchematics.get(player.world);
+                String playerUuid = player.getUniqueID().toString();
+
+                if(message.action == Action.GET_INFO) {
+                    Builders.NETWORK.sendToAllPlotServers(new PacketRequestSchemaInfo());
+                    return new S2CSchemaInfo(S2CSchemaInfo.Action.OVERWRITE, dataSchematics.getAllInfo());
+                } else if(message.action == Action.PRINT) {
+                    ContainerCreator schematicTerminal = (ContainerCreator)container;
+                    ItemStack paper = schematicTerminal.inventorySlots.get(1).getStack();
+
+                    if(!paper.isEmpty() && paper.getItem() == Items.PAPER) {
+                        ItemStack currentBlueprint = schematicTerminal.inventorySlots.get(2).getStack();
+                        BuildersSchematic schematic = dataSchematics.getSchematic(playerUuid, message.args[0]);
+
+                        if(currentBlueprint.isEmpty() && schematic != null) {
+                            ItemStack blueprint = new ItemStack(InitItem.BLUEPRINT, 1);
+                            ItemBlueprint.setSchematicNBT(blueprint, schematic);
+                            schematicTerminal.putStackInSlot(2, blueprint);
+                            paper.shrink(1);
+                            schematicTerminal.putStackInSlot(1, paper);
+                            schematicTerminal.detectAndSendChanges();
+                        }
+                    }
+                }
+            }
+
             return null;
         }
     }
