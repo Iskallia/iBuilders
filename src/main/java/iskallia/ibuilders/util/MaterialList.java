78d074eff13bd8893de17f6d194d722887add022
Unknown <kaptainwutax@gmail.com>
2019-11-23 14:56:36 -0500

    added fluids, farmland and path block overrides

--- src/main/java/iskallia/ibuilders/util/MaterialList.java
+++ src/main/java/iskallia/ibuilders/util/MaterialList.java
@@ -2,7 +2,10 @@ package iskallia.ibuilders.util;
 
 import iskallia.ibuilders.Builders;
 import net.minecraft.block.BlockSlab;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -13,6 +16,17 @@ import java.util.List;
 public class MaterialList {
 
     public static final List<Behaviour> OVERRIDES = new ArrayList<>();
+
+    //Convert fluids to the associated bucket.
+    public static Behaviour FLUIDS;
+    //Convert farmland and path blocks to dirt.
+    public static Behaviour FARMLAND_AND_PATH;
+    //Double slab blocks require 2 of the associated slab items.
+    public static Behaviour DOUBLE_SLAB;
+    //Runs the pick block routine.
+    public static Behaviour PICK_BLOCK;
+    //Converts the block to it's associated item block.
+    public static Behaviour ITEM_BLOCK;
 
     public static ItemStack getItem(World world, IBlockState state, BlockPos pos) {
         initializeOverrides();
@@ -31,7 +45,38 @@ public class MaterialList {
     public static void initializeOverrides() {
         OVERRIDES.clear();
 
-        OVERRIDES.add(new Behaviour() {
+        OVERRIDES.add(FLUIDS = new Behaviour() {
+            @Override
+            public boolean matches(IBlockState state) {
+                return state.getMaterial().isLiquid();
+            }
+
+            @Override
+            public ItemStack getItem(World world, IBlockState state, BlockPos pos) {
+                //TODO: Not hardcoding this...
+                if(state.getMaterial() == Material.WATER) {
+                    return new ItemStack(Items.WATER_BUCKET, 1);
+                } else if(state.getMaterial() == Material.LAVA) {
+                    return new ItemStack(Items.LAVA_BUCKET, 1);
+                }
+
+                return ItemStack.EMPTY;
+            }
+        });
+
+        OVERRIDES.add(FARMLAND_AND_PATH = new Behaviour() {
+            @Override
+            public boolean matches(IBlockState state) {
+                return state.getBlock() == Blocks.FARMLAND || state.getBlock() == Blocks.GRASS_PATH;
+            }
+
+            @Override
+            public ItemStack getItem(World world, IBlockState state, BlockPos pos) {
+                return new ItemStack(Blocks.DIRT, 1);
+            }
+        });
+
+        OVERRIDES.add(DOUBLE_SLAB = new Behaviour() {
             @Override
             public boolean matches(IBlockState state) {
                 return state.getBlock() instanceof BlockSlab && ((BlockSlab)state.getBlock()).isDouble();
@@ -45,7 +90,7 @@ public class MaterialList {
             }
         });
 
-        OVERRIDES.add(new Behaviour() {
+        OVERRIDES.add(PICK_BLOCK = new Behaviour() {
             @Override
             public boolean matches(IBlockState state) {
                 return true;
@@ -62,7 +107,7 @@ public class MaterialList {
             }
         });
 
-        OVERRIDES.add(new Behaviour() {
+        OVERRIDES.add(ITEM_BLOCK = new Behaviour() {
             @Override
             public boolean matches(IBlockState state) {
                 return true;
